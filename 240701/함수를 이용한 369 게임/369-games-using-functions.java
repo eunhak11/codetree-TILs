d587822cf56ef61feb45dc0b51766afe6f54c268
eunhak11 <102153071+eunhak11@users.noreply.github.com>
2024-07-01 20:01:49 +0900

    Title: 함수를 이용한 369 게임, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240701/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int cnt=0;
+        for(int i=a; i<=b; i++){
+            if(theNumber(i)) cnt++;
+        }
+    }
+
+    public static boolean theNumber(int n){
+        if(isTsn(n) || isMult(n)) return true;
+        else return false;
+    }
+    public static boolean isTsn(int n){
+        while((n/10)!=0){
+            if((n%10)%3==0) return true;
+            else{
+                n=n/10;
+            }
+        }
+        return false;
+    }
+    public static boolean isMult(int n){
+        if(n%3==0) return true;
+        else return false;
+    }
+}
