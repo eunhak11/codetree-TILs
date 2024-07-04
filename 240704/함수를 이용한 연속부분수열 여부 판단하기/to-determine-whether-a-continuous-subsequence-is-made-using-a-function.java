ad01508571ffa0151f5470478b5d9abb6b66d83d
eunhak11 <102153071+eunhak11@users.noreply.github.com>
2024-07-04 19:23:19 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240704/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240704/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -37,6 +37,7 @@ public class Main {
                         }
                         idc++;
                     }
+                    if(idc>0) return true;
                 }
             }
             if(idc>0) return true;
