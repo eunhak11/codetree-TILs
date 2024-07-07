d2e96e51e3194c674301f946a88d88d034168a43
LEH11 <102153071+eunhak11@users.noreply.github.com>
2024-07-07 23:47:30 +0900

    Title: 증가시키며 출력하기 2, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240707/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        while(a<27){
+            System.out.printf("%d ", a);
+            a++;
+        }
+    }
+}
